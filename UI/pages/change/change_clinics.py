d0d11d59d14ee7532516eefbedd233d1d45e1b57
lilfuz <lilfuz@gmail.com>
2024-02-06 17:35:48 +0300

    work buttons (change)

--- UI/pages/change/change_clinics.py
+++ UI/pages/change/change_clinics.py
@@ -6,17 +6,25 @@
 from flet import *
 from flet_navigator import PageData
 from UI.sidebar import SideBar
+from modules.load_data import LoadDropBox, LoadPages
+from modules.process_data import ProcessData
+
+
 #################################################
 
 
-class ContentFilled(UserControl):
-    def __init__(self):
+class Content(UserControl):
+    def __init__(self, load_drop_box, buttons, pg, db, process_data, load_pages=None, row_id=None):
         super().__init__()
-
-
-class ContentEmpty(UserControl):
-    def __init__(self):
-        super().__init__()
+        self.__dlg_modal = None
+        self.__existed_data = []
+        self.__load_drop_box = load_drop_box
+        self.__load_pages = load_pages
+        self.__process_data = process_data
+        self.__row_id = row_id
+        self.__data = buttons
+        self.__pg = pg
+        self.__db = db
 
     def build(self):
         name = TextField(label='Название')
@@ -168,11 +176,19 @@ class ContentEmpty(UserControl):
 class change_clinics:
     def __init__(self, vault, config, db):
         super(change_clinics, self).__init__()
+        self.__states = None
         self.__vault = vault
         self.__config = config
-        self.__states = {'add': ContentEmpty(), 'change': ContentFilled()}
+        self.__db = db
+        self.__load_drop_box = LoadDropBox(db)
+        self.__load_pages = LoadPages(db)
+        self.__process_data = ProcessData()
 
     def change_clinics(self, pg: PageData):
+        row_id = pg.page.client_storage.get("current_action")[1]
+        self.__states = {'add': Content(self.__load_drop_box, self.__data_buttons, pg, self.__db, self.__process_data),
+                         'change': Content(self.__load_drop_box, self.__data_buttons, pg, self.__db,
+                                           self.__process_data, self.__load_pages, row_id)}
         pg.page.title = "Клиники - Создать"
         pg.page.theme_mode = 'dark'
         pg.page.vertical_alignment = MainAxisAlignment.CENTER
@@ -194,7 +210,7 @@ class change_clinics:
                     Container(
                         border_radius=10,
                         expand=True,
-                        content=self.__states[pg.page.client_storage.get("current_action")],
+                        content=self.__states[pg.page.client_storage.get("current_action")[0]],
                         shadow=BoxShadow(
                             spread_radius=1,
                             blur_radius=15,
